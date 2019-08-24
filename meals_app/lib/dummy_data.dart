e4290db02d1517e9d69e1458826d20716a0f6b70
CesarCedanoV <CesarCedanoV@gmail.com>
2019-08-24 09:01:49 -0400

    Created the Category models and then created  a list of categories with Dummy data

---
+++ meals_app/lib/dummy_data.dart
@@ -0,0 +1,15 @@
+import './models/category.dart';
+import 'package:flutter/material.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(id: 'c1', title: 'Dominican Food', color: Colors.green),
+  Category(id: 'c2', title: 'Fast Food', color: Colors.blue),
+  Category(id: 'c3', title: 'Sea Food', color: Colors.yellow),
+  Category(id: 'c4', title: 'Asian Food', color: Colors.red),
+  Category(id: 'c5', title: 'Vegan', color: Colors.purple),
+  Category(id: 'c6', title: 'Desert', color: Colors.orange),
+  Category(id: 'c7', title: 'Salad', color: Colors.pink),
+  Category(id: 'c8', title: 'italian Food', color: Colors.teal),
+  Category(id: 'c9', title: 'c9', color: Colors.cyan),
+  Category(id: 'c10', title: 'c10', color: Colors.amberAccent),
+];
